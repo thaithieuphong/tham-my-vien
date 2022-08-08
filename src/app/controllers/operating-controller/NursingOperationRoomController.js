328b75b42281edb97d9682683a9e160d9948b400
thieuphongthai <thaithieu.phong119@gmail.com>
2022-08-08 09:46:16 +0700

    fix conflic v1

--- src/app/controllers/operating-controller/NursingOperationRoomController.js
+++ src/app/controllers/operating-controller/NursingOperationRoomController.js
@@ -17,50 +17,6 @@ class NursingController {
 	}
 
 	showServiceNote(req, res, next) {
-<<<<<<< HEAD
-        ServiceNote.find({ stored: "No", status: "Đang xử lý", nursing: req.userId } ).populate('recept').populate('customerID').populate('performer').populate('nursing')
-        .then((serviceNote) => {
-            console.log(serviceNote.customerID)
-            const cln = [];
-            serviceNote.forEach(element => {
-                let clns = element.counselorName;
-                for( const element of clns){
-                    cln.push(element);
-                }
-                return cln;
-
-            })
-            Counselor.find({ filename: {$in: cln} })
-                .then((counselors) => {
-                    res.render("operating/nursing/operating-service-note", {
-                        serviceNote:  multipleMongooseToObject(serviceNote),
-                        counselors: multipleMongooseToObject(counselors), 
-                        title: "Chi tiết khách hàng"
-                    });
-                })
-        })
-        .catch(next);
-    }
-
-    updateServiceNote(req, res, next) {
-        // res.json(req.body)
-        // console.log(req.params.id)
-        Promise.all([
-            ServiceNote.find({_id: req.params.id}).updateOne({$set: {status: "Hoàn thành"}}),
-            User.updateMany({_id: {$in: req.body.operatingID}},{$set: {state:"Medium"}})
-        ])
-                .then((serviceNote) => {
-                    console.log("serviceNote:",serviceNote)
-                    res.redirect('back')
-                })
-                .catch(next);
-    }
-
-    uploadBefore(req, res, next){
-        console.log("req", req)
-        res.redirect('back')
-    }
-=======
 		ServiceNote.find({ stored: "No", status: "Đang xử lý", nursing: req.userId }).populate('recept').populate('customerID').populate('performer').populate('nursing')
 
 			.then((serviceNote) => {
@@ -187,8 +143,6 @@ class NursingController {
 			})
 			.catch(next);
 	}
->>>>>>> origin/vinh
-
 }
 
 module.exports = new NursingController;
--- src/middleware/validateUpload.js
+++ src/middleware/validateUpload.js
@@ -1,9 +1,8 @@
-<<<<<<<< HEAD:src/middleware/validateUpload.js
 const multer = require('multer');
 const helpers = require('./helpers');
 const storage = require('./storage');
 
-class validateUploadImage {
+class validateUpload {
     
     // 'profile_pic' is the name of our file input field in the HTML form
     uploadSingleCustomer = multer({ fileFilter: helpers.imageFilter, storage: storage.storageCustomerAvt }).single('image');
@@ -15,24 +14,4 @@ class validateUploadImage {
     reExaminationUpload = multer({  fileFilter: helpers.filter, storage: storage.reExaminationUpload }).array('reExamination');
 }
 
-module.exports = new validateUploadImage;
-========
-const multer = require('multer');
-const helpers = require('./helpers');
-const storage = require('./storage');
-
-class validateUploadImage {
-    
-    // 'profile_pic' is the name of our file input field in the HTML form
-    uploadSingleCustomer = multer({ fileFilter: helpers.imageFilter, storage: storage.storageCustomerAvt }).single('image');
-    uploadSingleUser = multer({ fileFilter: helpers.imageFilter, storage: storage.storageUserAvt }).single('image');
-    uploadSingleUserEdit = multer({ fileFilter: helpers.imageFilter, storage: storage.storageUserAvtEdit }).single('image');
-    counselorUploadGoogleDrive = multer({ fileFilter: helpers.videoFilter, storage: storage.counselorUpload }).array('counselor');
-    beforeUploadImg = multer({ fileFilter: helpers.imageFilter, storage: storage.beforeUpload }).array('before');
-
-    afterUploadGoogleDrive = multer({ fileFilter: helpers.videoFilter, storage: storage.afterUpload }).array('after');
-    reExaminationUploadGoogleDrive = multer({  fileFilter: helpers.videoFilter, storage: storage.reExaminationUpload }).array('reExamination');
-}
-
-module.exports = new validateUploadImage;
->>>>>>>> origin/vinh:src/middleware/validateUploadImage.js
+module.exports = new validateUpload;
